b943d022cd3e5dbbed2fecc21f333f98160c8693
zenOfBass <108999864+zenOfBass@users.noreply.github.com>
2026-06-10 10:45:00 -0500

    Adding fizzbuzz as an example and connecting error messages

--- src/App/Program.cs
+++ src/App/Program.cs
@@ -6,10 +6,17 @@ var source = args.Length > 0
     ? File.ReadAllText(args[0])
     : Console.In.ReadToEnd();
 
-var tokens  = new Lexer(source).Tokenize();
-var program = new Parser(tokens).Parse();
-new TypeChecker().Check(program);
-RunOnLargeStack(() => new Interpreter().Execute(program));
+try
+{
+    var tokens  = new Lexer(source).Tokenize();
+    var program = new Parser(tokens).Parse();
+    new TypeChecker().Check(program);
+    RunOnLargeStack(() => new Interpreter().Execute(program));
+}
+catch (LexerException e)      { Console.Error.WriteLine(e.Message); Environment.Exit(1); }
+catch (ParseException e)      { Console.Error.WriteLine(e.Message); Environment.Exit(1); }
+catch (TypeException e)       { Console.Error.WriteLine(e.Message); Environment.Exit(1); }
+catch (RuntimeException e)    { Console.Error.WriteLine(e.Message); Environment.Exit(1); }
 
 static void RunOnLargeStack(Action action)
 {
